74e56e55f0c3b4f7cde87f60c667fdbfda7b237e
David Thanh Lam <davidlam021@gmail.com>
2019-10-13 22:39:43 -0700

    Added the bottom navigation bar and can switch between activities with it.

--- app/src/main/java/com/example/moodmixer/SongsListActivity.java
+++ app/src/main/java/com/example/moodmixer/SongsListActivity.java
@@ -1,8 +1,13 @@
 package com.example.moodmixer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SongsListActivity extends AppCompatActivity {
 
@@ -10,5 +15,51 @@ public class SongsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs_list);
+
+        //Code for bottom navigation view
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    //Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_music_viewer:
+                            //selectedFragment = new MusicPlayerActivity();
+                            openMusicPlayerActivity();
+                            break;
+                        case R.id.nav_playlist:
+                            openPlaylistActivity();
+                            //selectedFragment = new PlaylistActivity();
+                            break;
+                        case R.id.nav_songlist:
+                            openSongsListActivity();
+                            //selectedFragment = new SongsListActivity();
+                            break;
+                    }
+
+                    //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    //      selectedFragment).commit();
+                    //Want to select clicked item
+                    return true;
+                }
+            };
+
+
+    public void openPlaylistActivity(){
+        Intent intent = new Intent(this, PlaylistActivity.class);
+        startActivity(intent);
+    }
+    public void openMusicPlayerActivity(){
+        Intent intent = new Intent(this, MusicPlayerActivity.class);
+        startActivity(intent);
+    }
+    public void openSongsListActivity(){
+        Intent intent = new Intent(this, SongsListActivity.class);
+        startActivity(intent);
     }
 }
