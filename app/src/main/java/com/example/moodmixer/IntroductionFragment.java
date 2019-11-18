99c58f9de295b035992a74b925eb043755954a58
Elliott Diaz <Elliott.Diaz1@gmail.com>
2019-11-18 10:01:04 -0800

    IntroductionFragment Created - Status In Progress

---
+++ app/src/main/java/com/example/moodmixer/IntroductionFragment.java
@@ -0,0 +1,48 @@
+package com.example.moodmixer;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+public class IntroductionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_music_player, null);
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
